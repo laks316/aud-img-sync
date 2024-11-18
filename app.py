fd6a9e888323bc2808c8ccbcec3da66b6e847258
laks316 <laks316@gmail.com>
2024-11-19 00:41:22 +0530

    Update app.py with temp fold

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def main():
         try:
             #st.write(os.getcwd())
             #os.system('rm -rf temmp_fold')
-            #os.mkdir('temmp_fold')
+            os.mkdir('temmp_fold')
             #with tempfile.TemporaryDirectory() as temp_dir:
             temp_dir = 'temmp_fold'
             #with temp_dir1 as temp_dir:  
